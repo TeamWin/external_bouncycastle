baikalos/android_external_bouncycastle

Add classes need by framework to core.platform.api.stubs
Tested by replacing bouncycastle dependency in framework target with
dependency on core.platform.api.stubs dependency and running build.

Bug: 113148576
Test: see above
Change-Id: I1640d16e9e76389fd9fae281b191f022b71fd3ae

srcgen/src/main/java/com/android/bouncycastle/srcgen/BouncyCastleTransform.java
@@ -15,18 +15,19 @@
  */
 package com.android.bouncycastle.srcgen;
 
+import static com.google.currysrc.api.process.ProcessorSupport.createMandatoryRule;
+import static com.google.currysrc.api.process.ProcessorSupport.createOptionalRule;
+
 import com.google.currysrc.Main;
 import com.google.currysrc.api.Rules;
-import com.google.currysrc.api.input.CompoundDirectoryInputFileGenerator;
 import com.google.currysrc.api.input.DirectoryInputFileGenerator;
 import com.google.currysrc.api.input.InputFileGenerator;
-import com.google.currysrc.api.match.SourceMatchers;
 import com.google.currysrc.api.output.BasicOutputSourceFileGenerator;
 import com.google.currysrc.api.output.OutputSourceFileGenerator;
-import com.google.currysrc.api.process.DefaultRule;
-import com.google.currysrc.api.process.Processor;
+import com.google.currysrc.api.process.ProcessorSupport;
 import com.google.currysrc.api.process.Rule;
 import com.google.currysrc.api.process.ast.TypeLocator;
+import com.google.currysrc.processors.AddAnnotation;
 import com.google.currysrc.processors.HidePublicClasses;
 import com.google.currysrc.processors.InsertHeader;
 import com.google.currysrc.processors.ModifyQualifiedNames;
@@ -34,10 +35,14 @@ import com.google.currysrc.processors.ModifyStringLiterals;
 import com.google.currysrc.processors.RenamePackage;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
 
 /**
  * Generates bouncycastle sources in the com.android.org.bouncycastle package.
@@ -51,18 +56,36 @@ public class BouncyCastleTransform {
      * java BouncyCastleTransform {source dir} {target dir}
      */
     public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+          throw new IllegalArgumentException(
+              "Usage: " + BouncyCastleTransform.class.getCanonicalName()
+                  + " <source-dir> <target-dir> <core-platform-api-file>");
+        }
         String sourceDir = args[0];
         String targetDir = args[1];
-        new Main(false /* debug */).execute(new BouncyCastleRules(sourceDir, targetDir));
+        Path corePlatformApiFile = Paths.get(args[2]);
+
+        Map<String, String> options = JavaCore.getOptions();
+        options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+        options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8);
+        options.put(JavaCore.COMPILER_DOC_COMMENT_SUPPORT, JavaCore.ENABLED);
+        options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_CHAR, JavaCore.SPACE);
+        options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_SIZE, "4");
+
+        new Main(false /* debug */)
+            .setJdtOptions(options)
+            .execute(new TransformRules(sourceDir, targetDir, corePlatformApiFile));
     }
 
-    static class BouncyCastleRules implements Rules {
+    static class TransformRules implements Rules {
         private final String sourceDir;
         private final String targetDir;
+        private final Path corePlatformApiFile;
 
-        BouncyCastleRules(String sourceDir, String targetDir) {
+        TransformRules(String sourceDir, String targetDir, Path corePlatformApiFile) {
             this.sourceDir = sourceDir;
             this.targetDir = targetDir;
+            this.corePlatformApiFile = corePlatformApiFile;
         }
 
         @Override
@@ -74,7 +97,8 @@ public class BouncyCastleTransform {
         public List<Rule> getRuleList(File ignored) {
             return Arrays.asList(
                     // Doc change: Insert a warning about the source code being generated.
-                    createMandatoryRule(new InsertHeader("/* GENERATED SOURCE. DO NOT MODIFY. */\n")),
+                    createMandatoryRule(
+                            new InsertHeader("/* GENERATED SOURCE. DO NOT MODIFY. */\n")),
                     // AST change: Change the package of each CompilationUnit
                     createMandatoryRule(new RenamePackage(ORIGINAL_PACKAGE, ANDROID_PACKAGE)),
                     // AST change: Change all qualified names in code and javadoc.
@@ -82,16 +106,17 @@ public class BouncyCastleTransform {
                     // AST change: Change all string literals containing package names in code.
                     createOptionalRule(new ModifyStringLiterals(ORIGINAL_PACKAGE, ANDROID_PACKAGE)),
                     // Doc change: Insert @hide on all public classes.
-                    createHidePublicClassesRule()
+                    createHidePublicClassesRule(),
+                    // AST change: Add CorePlatformApi to specified classes and members
+                    createOptionalRule(new AddAnnotation("libcore.api.CorePlatformApi",
+                        ProcessorSupport.readBodyDeclarationLocations(corePlatformApiFile)))
                     );
         }
 
         private static Rule createHidePublicClassesRule() {
             List<TypeLocator> publicApiClassesWhitelist = Collections.emptyList();
-            return createOptionalRule(
-                new HidePublicClasses(
-                        publicApiClassesWhitelist,
-                        "This class is not part of the Android public SDK API"));
+            return createOptionalRule(new HidePublicClasses(publicApiClassesWhitelist,
+                    "This class is not part of the Android public SDK API"));
         }
 
         @Override
@@ -99,14 +124,6 @@ public class BouncyCastleTransform {
             File outputDir = new File(targetDir);
             return new BasicOutputSourceFileGenerator(outputDir);
         }
-    }
-
-    public static DefaultRule createMandatoryRule(Processor processor) {
-        return new DefaultRule(processor, SourceMatchers.all(), true /* mustModify */);
-    }
-
-    public static DefaultRule createOptionalRule(Processor processor) {
-        return new DefaultRule(processor, SourceMatchers.all(), false /* mustModify */);
     }
 
     private BouncyCastleTransform() {

TeamWin/external_bouncycastle

srcgen/src/main/java/com/android/bouncycastle/srcgen/BouncyCastleTransform.java
@@ -15,18 +15,19 @@
  */
 package com.android.bouncycastle.srcgen;
 
+import static com.google.currysrc.api.process.Rules.createMandatoryRule;
+import static com.google.currysrc.api.process.Rules.createOptionalRule;
+
 import com.google.currysrc.Main;
-import com.google.currysrc.api.Rules;
-import com.google.currysrc.api.input.CompoundDirectoryInputFileGenerator;
+import com.google.currysrc.api.RuleSet;
 import com.google.currysrc.api.input.DirectoryInputFileGenerator;
 import com.google.currysrc.api.input.InputFileGenerator;
-import com.google.currysrc.api.match.SourceMatchers;
 import com.google.currysrc.api.output.BasicOutputSourceFileGenerator;
 import com.google.currysrc.api.output.OutputSourceFileGenerator;
-import com.google.currysrc.api.process.DefaultRule;
-import com.google.currysrc.api.process.Processor;
 import com.google.currysrc.api.process.Rule;
+import com.google.currysrc.api.process.ast.BodyDeclarationLocators;
 import com.google.currysrc.api.process.ast.TypeLocator;
+import com.google.currysrc.processors.AddAnnotation;
 import com.google.currysrc.processors.HidePublicClasses;
 import com.google.currysrc.processors.InsertHeader;
 import com.google.currysrc.processors.ModifyQualifiedNames;
@@ -34,10 +35,14 @@ import com.google.currysrc.processors.ModifyStringLiterals;
 import com.google.currysrc.processors.RenamePackage;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
 
 /**
  * Generates bouncycastle sources in the com.android.org.bouncycastle package.
@@ -51,18 +56,36 @@ public class BouncyCastleTransform {
      * java BouncyCastleTransform {source dir} {target dir}
      */
     public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+          throw new IllegalArgumentException(
+              "Usage: " + BouncyCastleTransform.class.getCanonicalName()
+                  + " <source-dir> <target-dir> <core-platform-api-file>");
+        }
         String sourceDir = args[0];
         String targetDir = args[1];
-        new Main(false /* debug */).execute(new BouncyCastleRules(sourceDir, targetDir));
+        Path corePlatformApiFile = Paths.get(args[2]);
+
+        Map<String, String> options = JavaCore.getOptions();
+        options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+        options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8);
+        options.put(JavaCore.COMPILER_DOC_COMMENT_SUPPORT, JavaCore.ENABLED);
+        options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_CHAR, JavaCore.SPACE);
+        options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_SIZE, "4");
+
+        new Main(false /* debug */)
+            .setJdtOptions(options)
+            .execute(new TransformRuleSet(sourceDir, targetDir, corePlatformApiFile));
     }
 
-    static class BouncyCastleRules implements Rules {
+    static class TransformRuleSet implements RuleSet {
         private final String sourceDir;
         private final String targetDir;
+        private final Path corePlatformApiFile;
 
-        BouncyCastleRules(String sourceDir, String targetDir) {
+        TransformRuleSet(String sourceDir, String targetDir, Path corePlatformApiFile) {
             this.sourceDir = sourceDir;
             this.targetDir = targetDir;
+            this.corePlatformApiFile = corePlatformApiFile;
         }
 
         @Override
@@ -74,7 +97,8 @@ public class BouncyCastleTransform {
         public List<Rule> getRuleList(File ignored) {
             return Arrays.asList(
                     // Doc change: Insert a warning about the source code being generated.
-                    createMandatoryRule(new InsertHeader("/* GENERATED SOURCE. DO NOT MODIFY. */\n")),
+                    createMandatoryRule(
+                            new InsertHeader("/* GENERATED SOURCE. DO NOT MODIFY. */\n")),
                     // AST change: Change the package of each CompilationUnit
                     createMandatoryRule(new RenamePackage(ORIGINAL_PACKAGE, ANDROID_PACKAGE)),
                     // AST change: Change all qualified names in code and javadoc.
@@ -82,16 +106,17 @@ public class BouncyCastleTransform {
                     // AST change: Change all string literals containing package names in code.
                     createOptionalRule(new ModifyStringLiterals(ORIGINAL_PACKAGE, ANDROID_PACKAGE)),
                     // Doc change: Insert @hide on all public classes.
-                    createHidePublicClassesRule()
+                    createHidePublicClassesRule(),
+                    // AST change: Add CorePlatformApi to specified classes and members
+                    createOptionalRule(new AddAnnotation("libcore.api.CorePlatformApi",
+                        BodyDeclarationLocators.readBodyDeclarationLocators(corePlatformApiFile)))
                     );
         }
 
         private static Rule createHidePublicClassesRule() {
             List<TypeLocator> publicApiClassesWhitelist = Collections.emptyList();
-            return createOptionalRule(
-                new HidePublicClasses(
-                        publicApiClassesWhitelist,
-                        "This class is not part of the Android public SDK API"));
+            return createOptionalRule(new HidePublicClasses(publicApiClassesWhitelist,
+                    "This class is not part of the Android public SDK API"));
         }
 
         @Override
@@ -99,14 +124,6 @@ public class BouncyCastleTransform {
             File outputDir = new File(targetDir);
             return new BasicOutputSourceFileGenerator(outputDir);
         }
-    }
-
-    public static DefaultRule createMandatoryRule(Processor processor) {
-        return new DefaultRule(processor, SourceMatchers.all(), true /* mustModify */);
-    }
-
-    public static DefaultRule createOptionalRule(Processor processor) {
-        return new DefaultRule(processor, SourceMatchers.all(), false /* mustModify */);
     }
 
     private BouncyCastleTransform() {